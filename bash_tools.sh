2fbae3711887e34c276e4480afd3c864f99318b4
Donaldttt <tang.1208@osu.edu>
2022-06-18 03:38:15 -0400

    change fn name

--- bash_tools.sh
+++ bash_tools.sh
@@ -1,11 +1,11 @@
+
 # args:
 # $1: 
 # The IP address you want to query geo info for.
 # If no argument supplied, the host machine's ip will
 # be used
 # doc for api: https://ip-api.com/docs/api
-ipinfo()
-{
+ipinfo(){
     api_str=http://ip-api.com/json/$1?fields=query,country,regionName,city,district,zip,isp,org,reverse,mobile,proxy,hosting
     if ! hash python; then
         curl $api_str
@@ -28,13 +28,13 @@ odict(){
 # Linux specific functions
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
-change_wallpaper(){
-    local file=$1
-    # convert to abs path
-    if [[ ! "$file" = /* ]]; then
-        file=$(pwd)/$1
-    fi
-    gsettings set org.gnome.desktop.background picture-uri file://$file
+    set_wallpaper(){
+        local file=$1
+        # convert to abs path
+        if [[ ! "$file" = /* ]]; then
+            file=$(pwd)/$1
+        fi
+        gsettings set org.gnome.desktop.background picture-uri file://$file
 }
 
 fi
@@ -42,26 +42,17 @@ fi
 # Mac OSX specific functions
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
-change_wallpaper(){
-    local file=$1
-    # convert to abs path
-    if [[ ! "$file" = /* ]]; then
-        file=$(pwd)/$1
-    fi
+    set_wallpaper(){
+        local file=$1
+        # convert to abs path
+        if [[ ! "$file" = /* ]]; then
+            file=$(pwd)/$1
+        fi
 
-    # not working for all desktops
-    #apple_script_all_desk=" \
-    #    import AppKit; \
-    #    let url = URL(fileURLWithPath: \"$file\"); \
-    #    for screen in NSScreen.screens {
-    #        try! NSWorkspace.shared.setDesktopImageURL(url, for: screen, options: [:]) \
-    #    }" 
-    #swift <<< $apple_script_all_desk
+        apple_script_single_desk="tell application \"Finder\" to set desktop picture to POSIX file \"$file\""
+        osascript -e "$apple_script_single_desk"
+    }
 
-    apple_script_single_desk="tell application \"Finder\" to set desktop picture to POSIX file \"$file\""
-    osascript -e "$apple_script_single_desk"
-
-}
 fi
 
 #
